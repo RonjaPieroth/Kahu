e34c269772d24052dc853f7816abc505c80040fb
RonjaPieroth <ronja.pieroth@gmail.com>
2024-12-16 13:12:44 +0100

    +structure for the navbar +profiletext data changed in backend

---
+++ kahu-frontend/src/app/future-pet-owner-profile/pet-owner-mailbox/pet-owner-mailbox.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-owner-mailbox',
+  templateUrl: './pet-owner-mailbox.component.html',
+  styleUrl: './pet-owner-mailbox.component.css'
+})
+export class PetOwnerMailboxComponent {
+
+}
---
+++ kahu-frontend/src/app/future-pet-owner-profile/swipe-page/swipe-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-swipe-page',
+  templateUrl: './swipe-page.component.html',
+  styleUrl: './swipe-page.component.css'
+})
+export class SwipePageComponent {
+
+}
