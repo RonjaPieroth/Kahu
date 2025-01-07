f680ef057007948a5c2e37627b335dcde65eb224
RonjaPieroth <ronja.pieroth@gmail.com>
2025-01-07 11:59:51 +0100

    + modal for pet profile while on browser component + connection between matches and pet profiles

--- kahu-frontend/src/app/shelter-profile/pets/pet-profile/pet-profile.component.ts
+++ kahu-frontend/src/app/shelter-profile/pets/pet-profile/pet-profile.component.ts
@@ -1,8 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Pet} from '../../../models/pet';
 import {PetService} from '../../../services/pet.service';
 import {ActivatedRoute} from '@angular/router';
 import {PetOwnershipType} from '../../../models/enums/pet-ownership-type';
+import {LoginService} from '../../../services/login.service';
+import {PetOwner} from '../../../models/pet-owner';
+import {Shelter} from '../../../models/shelter';
 
 @Component({
   selector: 'app-pet-profile',
@@ -11,14 +14,28 @@ import {PetOwnershipType} from '../../../models/enums/pet-ownership-type';
 })
 export class PetProfileComponent {
 
-  pet?: Pet;
+  @Input() pet?: Pet;
+  visitingProfile?: PetOwner|Shelter;
 
-  constructor(private petService: PetService, private route: ActivatedRoute) {
+
+  constructor(private petService: PetService, private route: ActivatedRoute, private loginService: LoginService) {
     const id = route.snapshot.paramMap.get("id");
 
-    if (id) {
+    if (!this.pet && id) {
       this.petService.getPetByID(id).subscribe(data => this.pet = data);
     }
+    this.loginService.getProfile().subscribe(data =>
+    this.visitingProfile = data.profile);
+  }
+
+  get isOwningShelter(): boolean{
+    if (this.visitingProfile?.id != this.pet?.shelter.id){return false;}
+    return !this.loginService.isPetOwner(this.visitingProfile);
+  }
+
+  get isMatch(): boolean{
+    return !!(this.loginService.isPetOwner(this.visitingProfile) && this.visitingProfile.matches.find(match => match.id === this.pet?.id));
+
   }
 
   protected readonly PetOwnershipType = PetOwnershipType;
