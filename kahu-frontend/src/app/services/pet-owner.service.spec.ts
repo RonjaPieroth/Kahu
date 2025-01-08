9357cf9d8f80eb216b462bca0341ee39c9e31330
RonjaPieroth <ronja.pieroth@gmail.com>
2025-01-08 09:48:37 +0100

    -small fix after renaming

--- kahu-frontend/src/app/services/pet-owner.service.spec.ts
+++ kahu-frontend/src/app/services/pet-owner.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UserService } from './pet-owner.service';
+import { PetOwnerService } from './pet-owner.service';
 
 describe('UserService', () => {
-  let service: UserService;
+  let service: PetOwnerService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    service = TestBed.inject(PetOwnerService);
   });
 
   it('should be created', () => {
